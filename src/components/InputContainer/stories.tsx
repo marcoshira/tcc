31b5fd981c17494a78793f46c7fc5e00cb7c4bd1
Marcos Hirazawa <hirazawa.marcos@gmail.com>
2022-11-03 19:27:33 -0300

    InputContainer clean

--- src/components/InputContainer/stories.tsx
+++ src/components/InputContainer/stories.tsx
@@ -1,10 +1,8 @@
 import { Meta, Story } from '@storybook/react/types-6-0';
 import { InputContainer, InputContainerProps } from '.';
 import { Heading } from '../Heading';
-import { InputDim } from '../InputDim';
 import { InputSoil } from '../InputSoil';
 import { InputSoilLayer } from '../InputSoilLayer';
-import { InputStake } from '../InputStake';
 import { Wrapper } from '../Wrapper';
 
 export default {
@@ -20,17 +18,14 @@ export default {
   },
 } as Meta<InputContainerProps>;
 
-export const Template: Story<InputContainerProps> = (args) => {
+export const Template: Story<InputContainerProps> = () => {
   return (
     <div>
       <Heading>JStake</Heading>
       <Wrapper>
-        <InputContainer
-          children={<InputSoilLayer depth={6} />}
-          title="Tipos de Solo"
-          frameSize="spt"
-          depth={16}
-        />
+        <InputContainer title="Tipos de Solo" frameSize="spt" depth={16}>
+          <InputSoilLayer depth={6} />
+        </InputContainer>
       </Wrapper>
     </div>
   );
