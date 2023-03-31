f34fdcbdb4d324fab22e40f776483af6638dc1a9
Marcos Hirazawa <hirazawa.marcos@gmail.com>
2023-03-31 17:32:23 -0300

    _document fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,12 +7,6 @@ import Head from 'next/head';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        ></meta>
-      </Head>
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
         <GlobalStyles />
